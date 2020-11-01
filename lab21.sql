72a2b06a3fa41254d30514055c450bfdd8764be2
Supriya Biswas <supriya0483@gmail.com>
2020-11-01 21:21:27 +0000

    Add files via upload

---
+++ lab21.sql
@@ -0,0 +1,7 @@
+rem lab21.sql
+SELECT  deriv_name, body, doors, auto
+	FROM	derivative
+	WHERE	model_no = 'm2';
+
+
+
---
+++ lab29.sql
@@ -0,0 +1,7 @@
+
+rem lab29.sql
+SELECT  make_name, model_name, length
+	FROM	model
+	WHERE	year_finish < 2004
+	ORDER BY length;
+
